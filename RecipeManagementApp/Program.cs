85c1fbe8f22a426588fd1c9753457852497b0605
Tiego Mathobela <ST10102873@rcconnect.edu.za>
2024-04-12 08:35:26 +0000

    Added Ingredient class to encapsulate ingredients the properties for name, quantity, and unit. Main method promting user for ingredients input Used array to store ingredients

--- RecipeManagementApp/Program.cs
+++ RecipeManagementApp/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*ST10102873
+ * PROG6211_PT1
+ * An Application to manage and organise recipes*/
+using RecipeManagementApp;
+
+Console.WriteLine("RECIPE MANAGEMENT!");
+
+//Declarations & intialising ==
+int numOfIngredients = 0;
+
+//Prompting user to enter number of ingredients:
+Console.Write("Enter number of ingredients:");
+numOfIngredients = Convert.ToInt32(Console.ReadLine());
+
+//An array to store ingredients
+Ingredients[] ingredients = new Ingredients[numOfIngredients];
+
+//Prompt user to enter details for each ingredient
+for (int i = 0; i < numOfIngredients; i++)
+{
+    Console.WriteLine($"\nEnter Ingredient (Name, Quantity, Unit) #{i + 1}:");
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Quantity: ");
+    double quantity = double.Parse(Console.ReadLine());
+    Console.Write("Unit: ");
+    string unit = Console.ReadLine();
+
+    //Ingredient object
+    ingredients[i] = new Ingredients { Name = name, Quantity = quantity, Unit = unit };
+}
+
+//temp print
+//Console.WriteLine("Ingredients:");
+//for (int i = 0; i < ingredients.Length; i++)
+//{
+//    Console.WriteLine($"{ingredients[i].Quantity} {ingredients[i].Unit} of {ingredients[i].Name}");
+//}
+
+
